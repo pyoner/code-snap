3e30e6e2e23d6929fd34eace89ef397a31fe64df
Askar Yusupov <devex.soft@gmail.com>
2025-06-24 14:41:07 +0500

    feat(lib.rs): implement `codesnap` function to generate image snapshots from code fix(lib.rs): add `ImageData` struct with getter for image byte data to facilitate JS interop

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,56 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
+use codesnap::{
+    config::{CodeBuilder, CodeSnap, Content},
+    snapshot::{image_snapshot::ImageSnapshot, snapshot_data::SnapshotData},
+};
+
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub struct ImageData {
+    pub width: usize,
+    pub height: usize,
+    data: Vec<u8>,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, code-snap!");
+impl ImageData {
+    #[wasm_bindgen(getter)]
+    pub fn data(&self) -> Vec<u8> {
+        self.data.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn codesnap(code: &str, language: &str, config: &str) -> ImageData {
+    let code_content = Content::Code(
+        CodeBuilder::default()
+            .content(code)
+            .language(language)
+            .build()
+            .unwrap(),
+    );
+
+    let snap_config = CodeSnap::from_config(config)
+        .unwrap()
+        .content(code_content)
+        .build()
+        .unwrap();
+
+    let image_data = ImageSnapshot::from_config(snap_config)
+        .unwrap()
+        .png_data()
+        .unwrap();
+
+    match image_data {
+        SnapshotData::Image {
+            data,
+            width,
+            height,
+        } => ImageData {
+            data,
+            width,
+            height,
+        },
+        _ => panic!("Expected image data but received different snapshot data"),
+    }
 }
