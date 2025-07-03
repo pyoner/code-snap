e5189979e60399da75c0d4de6dcc774287fe2d24
Askar Yusupov <devex.soft@gmail.com>
2025-07-03 16:53:42 +0500

    feat(index.ts): add type annotation for config parameter to improve type safety fix(index.ts): assign the imported function to a consistent variable name for clarity chore(tsconfig.json): extend configuration with detailed options for better type checking docs(types.ts): generate TypeScript types from JSON schema to ensure type safety and clarity

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+import type { SnapshotConfig } from "./types";
 // @ts-ignore
 import codeSnapWasmModule from "./code_snap_bg.wasm";
 // @ts-ignore
-import { codesnap, initSync } from "./code_snap";
+import { codesnap as fn, initSync } from "./code_snap";
 
 initSync({ module: codeSnapWasmModule });
+
+const codesnap = (code: string, language: string, config?: SnapshotConfig) =>
+  fn(code, language, config ? JSON.stringify(config) : null);
 export { codesnap };
