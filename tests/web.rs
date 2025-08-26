5c805d5dec226a128b9e3e0265d252f0c8a2c9de
Askar Yusupov <devex.soft@gmail.com>
2025-08-26 11:04:40 +0500

    chore(Cargo.toml): update package name from "codesnap" to "code-snap" for consistency test(web.rs): add tests for codesnap function with and without configuration to ensure correct behavior in browser environment

--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,39 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use code_snap::codesnap;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn test_codesnap_without_config_browser() {
+    let code = "fn main() { println!(\"Hello, world!\"); }";
+    let language = "rust";
+    let config = None;
+
+    let result = codesnap(code, language, config);
+
+    assert!(result.width > 0);
+    assert!(result.height > 0);
+    assert!(result.data().length() > 0);
+}
+
+#[wasm_bindgen_test]
+fn test_codesnap_with_config_browser() {
+    let code = "fn main() { println!(\"Hello, world!\"); }";
+    let language = "rust";
+    let config = Some(
+        r###"{
+            "theme": "candy",
+            "background": "#000000"
+        }"###
+            .to_string(),
+    );
+
+    let result = codesnap(code, language, config);
+
+    assert!(result.width > 0);
+    assert!(result.height > 0);
+    assert!(result.data().length() > 0);
 }
